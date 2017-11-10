59402a348b7c0f3001232bc19fa5a489319f56c6
mjwmeijer <mjwmeijer@yahoo.com>
2017-11-11 00:02:56 +0100

    https instead of http.

--- browser273.js
+++ browser273.js
@@ -1,9 +1,12 @@
+//dit werkt niet op github: er is geen
+
 $(document).ready(function (){
   $('#btnGetWeather').on('click',function(){
     var plaatsNaam = $('#txtCity').val();
-    var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q='+plaatsNaam+'&appid=3e7a4ae823f7aac5ffc36b2fc665c538';
+    var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+plaatsNaam+
+    '&units=metric&lang=nl&appid=3e7a4ae823f7aac5ffc36b2fc665c538';
 
-    // example: http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=3e7a4ae823f7aac5ffc36b2fc665c538
+
 
     $.ajax({
       url:weatherUrl,
@@ -11,6 +14,12 @@ $(document).ready(function (){
       contentType:'application/json',
       success:function(weather){
         console.log(weather);
+        //nog eens goed bekijken waarom href hier goed gaat!
+        var linkje = 'https://openweathermap.org/city/'+weather.coord.id;
+        var weerInformatie = '<strong><a href="' +linkje+ '" target="_blank">'+weather.name +
+        ' (' +weather.sys.country+ ')</a></strong>';
+        weerInformatie += ', temperatuur ' + weather.main.temp +' graden';
+        $('#divResult').append(weerInformatie);
       }
 
     })
