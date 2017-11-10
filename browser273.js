c12bb3d0db49a04b8d3fd9b29ed89b1eddcfc968
mjwmeijer <mjwmeijer@yahoo.com>
2017-11-10 19:56:24 +0100

    First draft weatherstation request page.

---
+++ browser273.js
@@ -0,0 +1,18 @@
+$(document).ready(function (){
+  $('#btnGetWeather').on('click',function(){
+    var plaatsNaam = $('#txtCity').val();
+    var weatherUrl = 'api.openweathermap.org/data/2.5/weather?q='+plaatsNaam+'&APPID=3e7a4ae823f7aac5ffc36b2fc665c538';
+    $.ajax({
+      url:weatherUrl,
+      dataType:'jsonp',
+      contentType:'application/json',
+      success:function(weather){
+        console.log(weather);
+      }
+
+    })
+
+
+});
+
+})
