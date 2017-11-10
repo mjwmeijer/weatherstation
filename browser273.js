d5ba0a2bf75ffa1ebae68ae7dc21e75e60971429
mjwmeijer <mjwmeijer@yahoo.com>
2017-11-10 21:36:07 +0100

    Add wunderground.

--- browser273.js
+++ browser273.js
@@ -3,8 +3,19 @@ $(document).ready(function (){
     var plaatsNaam = $('#txtCity').val();
     var weatherUrl = 'api.openweathermap.org/data/2.5/weather?q='+plaatsNaam+'&APPID=3e7a4ae823f7aac5ffc36b2fc665c538';
     $.ajax({
+      url: 'https://api.wunderground.com/api/4e1fe0d302980707/geolookup/conditions/q/NY/Manhattan.json',
+      datatype: 'jsonp',
+      success: function(data) {
+        console.log($('.weather').html('<p class="data">' +
+        data.current_observation.weather +'<br />' + data.current_observation.temp_c + ' degrees Celsius</p>'));
+      }
+    });
+
+
+    $.ajax({
       url:weatherUrl,
       dataType:'jsonp',
+      contentType:'application/json',
       success:function(weather){
         console.log(weather);
       }
