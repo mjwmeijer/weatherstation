2c6b0cfa950826ce4adcbdf68d0d15c1debb133a
mjwmeijer <mjwmeijer@yahoo.com>
2017-11-10 22:17:31 +0100

    removing API key.

--- browser273.js
+++ browser273.js
@@ -1,17 +1,8 @@
 $(document).ready(function (){
   $('#btnGetWeather').on('click',function(){
     var plaatsNaam = $('#txtCity').val();
-    var weatherUrl = 'api.openweathermap.org/data/2.5/weather?q='+plaatsNaam+'&APPID=3e7a4ae823f7aac5ffc36b2fc665c538';
-    $.ajax({
-      url: 'https://api.wunderground.com/api/4e1fe0d302980707/geolookup/conditions/q/NY/Manhattan.json',
-      datatype: 'jsonp',
-      success: function(data) {
-        console.log($('.weather').html('<p class="data">' +
-        data.current_observation.weather +'<br />' + data.current_observation.temp_c + ' degrees Celsius</p>'));
-      }
-    });
-
-
+    var weatherUrl = 'api.openweathermap.org/data/2.5/weather?q='+plaatsNaam;
+  
     $.ajax({
       url:weatherUrl,
       dataType:'jsonp',
