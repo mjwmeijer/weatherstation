beda176a51e2eee0685b6961ba7b33d94b7ac432
mjwmeijer <mjwmeijer@yahoo.com>
2017-11-10 22:26:25 +0100

    Adding http protocol.

--- browser273.js
+++ browser273.js
@@ -1,8 +1,10 @@
 $(document).ready(function (){
   $('#btnGetWeather').on('click',function(){
     var plaatsNaam = $('#txtCity').val();
-    var weatherUrl = 'api.openweathermap.org/data/2.5/weather?q='+plaatsNaam;
-  
+    var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q='+plaatsNaam+'&appid=3e7a4ae823f7aac5ffc36b2fc665c538';
+
+    // example: http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=3e7a4ae823f7aac5ffc36b2fc665c538
+
     $.ajax({
       url:weatherUrl,
       dataType:'jsonp',
